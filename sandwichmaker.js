e646c3a8d0a3b47615c5167742c9f2df4824c4cb
Debra Gordon <debra.m.gordon@gmail.com>
2016-09-03 13:56:25 -0500

    DONE

--- sandwichmaker.js
+++ sandwichmaker.js
@@ -1,6 +1,37 @@
 var yourSandwich = ( function (sandwich){
+  var sandwichNames = {
+  "Ham": "Piggy",
+  "Turkey": "Gobbler",
+  "Pepperoni": "Little Italy",
+  "Bologne": "Joker",
+  "Chicken": "Road Crosser",
+  "None": "Green Machine",
+  "TwoMeat": "Hefty",
+  "ThreeMeat": "Trucker",
+  "FourMeat": "Lumber Jack",
+  "FiveMeat": "Glutton"
+  }
+
   sandwich.getTotalSandwichPrice = function () {
     return sandwich.getTotalBreadPrice() + sandwich.getTotalCheesePrice() + sandwich.getTotalVeggiePrice() + sandwich.getTotalExtraPrice() + sandwich.getTotalMeatPrice();
+  }
+  sandwich.getSandwichName = function () {
+    var meats = sandwich.getSelectedMeats();
+    if (meats.length === 5) {
+      return sandwichNames.FiveMeat;
+    }
+    if (meats.length === 4) {
+      return sandwichNames.FourMeat;
+    }
+    if (meats.length === 3) {
+      return sandwichNames.ThreeMeat;
+    }
+    if (meats.length === 2) {
+      return sandwichNames.TwoMeat;
+    }
+    if (meats.length === 1) {
+      return sandwichNames[meats[0]]
+    }
   }
   return sandwich;
 })(yourSandwich || {});
@@ -11,29 +42,4 @@ var yourSandwich = ( function (sandwich){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sandwich names {
-//   ham: "Piggy",
-//   turkey: "Gobbler",
-//   pepperoni: "Little Italy",
-//   bologne: "Joker",
-//   chicken: "Road Crosser",
-//   none: "Green Machine",
-//   twomeat: "Hefty",
-//   threemeat: "Trucker",
-//   fourmeat: "Lumber Jack",
-//   fivemeat: "Glutton"
-// }
 
